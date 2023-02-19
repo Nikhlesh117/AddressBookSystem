f87e494639a73818a841e9c056d85332e906cee1
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-19 18:50:47 +0530

    Added: Calling UC2

--- Program.cs
+++ Program.cs
@@ -5,6 +5,37 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter city: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter state: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter zip code: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter phone number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("\n");
+
+
+            contact newContact = new contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
         }
     }
 }
