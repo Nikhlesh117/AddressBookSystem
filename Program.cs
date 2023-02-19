efc15b9526a555f3dd50074b58ffff0dde0e0d14
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-19 18:32:22 +0530

    Added: Calling UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Address Book System");
+
+            contact contact1 = new contact("Nick", "R", "Vskp", "Vizag", "AP", "12345", "1234567890", "nick@Email.com");
         }
     }
 }
