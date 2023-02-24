1f2f454249bc044453d6a7aed44f9daa58860563
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-24 11:01:58 +0530

    Added: Add Multiple Address Books

--- Program.cs
+++ Program.cs
@@ -1,10 +1,101 @@
-﻿namespace AddressBookSystem
+﻿using System.Diagnostics.Contracts;
+using System.Net;
+using System.Xml.Linq;
+
+namespace AddressBookSystem
 {
     public class Program
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to the Address Book System");
+            ContactManager contactManager = new ContactManager();
+            int choice, choice2;
+            string bookName = "default";
+            Console.WriteLine("Would You Like To \n1.Work on default AddressBook \n2.Create New AddressBook");
+            choice2 = Convert.ToInt32(Console.ReadLine());
+            switch (choice2)
+            {
+                case 1:
+                    contactManager.AddAddressBook(bookName);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Name Of New Addressbook You want to create : ");
+                    bookName = Console.ReadLine();
+                    contactManager.AddAddressBook(bookName);
+                    break;
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"Working On {bookName} AddressBook\n");
+                Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Add New AddressBook \n7.Switch AddressBook \n8.Exit Application\n");
+
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        contactManager.AddContact(bookName);
+                        break;
+                    case 2:
+                        string nameToEdit = Console.ReadLine();
+                        contactManager.EditContact(nameToEdit, bookName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter First Name Of Contact To Delete :");
+                        string nameToDelete = Console.ReadLine();
+                        contactManager.DeleteContact(nameToDelete, bookName);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter First Name Of Contact To View :");
+                        string nameToView = Console.ReadLine();
+                        contactManager.ViewContact(nameToView, bookName);
+                        break; 
+                    case 5:
+                        contactManager.ViewContact(bookName);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter Name For New AddressBook");
+                        string newAddressBook = Console.ReadLine();
+                        contactManager.AddAddressBook(newAddressBook);
+                        Console.WriteLine("Would you like to Switch to " + newAddressBook);
+                        Console.WriteLine("1.Yes \n2.No");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        if (option == 1)
+                        {
+                            bookName = newAddressBook;
+                        }
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter Name Of AddressBook From Below List");
+                        foreach (KeyValuePair<string, ContactManager> item in contactManager.GetAddressBook())
+                        {
+                            Console.WriteLine(item.Key);
+                        }
+                        while (true)
+                        {
+                            bookName = Console.ReadLine();
+                            if (contactManager.GetAddressBook().ContainsKey(bookName))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("No such AddressBook found. Try Again.");
+                            }
+                        }
+                        break;
+                    case 8:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
