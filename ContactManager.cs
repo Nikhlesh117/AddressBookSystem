608ab86140dd7b3bb37f8e7a5c28b4e2990f0dab
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-09 08:31:10 +0530

    Added: No Duplicate Entry

---
+++ ContactManager.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AddressBookSystem
+{
+    public class ContactManager:IContacts
+    {
+        //private List<Contact> contacts = new List<Contact>();
+        private Dictionary<string, Contact> contacts = new Dictionary<string, Contact>();
+        private Dictionary<string, ContactManager> addressBookDictionary = new Dictionary<string, ContactManager>();
+
+
+        public void AddContact(string bookName)
+        {
+            Console.WriteLine("Add a new contact");
+            Console.WriteLine("-----------------");
+
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Phone number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBookDictionary[bookName].contacts.Add(newContact.FirstName,newContact);
+
+            Console.WriteLine("Contact added successfully!");
+        }
+
+        public void ViewContact(string name, string bookName)
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBookDictionary[bookName].contacts)
+            {
+                if (item.Key == name)
+                {
+                    Console.WriteLine("First Name : " + item.Value.FirstName);
+                    Console.WriteLine("Last Name : " + item.Value.LastName);
+                    Console.WriteLine("Address : " + item.Value.Address);
+                    Console.WriteLine("City : " + item.Value.City);
+                    Console.WriteLine("State : " + item.Value.State);
+                    Console.WriteLine("Email : " + item.Value.Email);
+                    Console.WriteLine("Zip : " + item.Value.Zip);
+                    Console.WriteLine("Phone Number : " + item.Value.PhoneNumber + "\n");
+                }
+            }
+        }
+        public void ViewContact(string bookName)
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBookDictionary[bookName].contacts)
+            {
+                Console.WriteLine("First Name : " + item.Value.FirstName);
+                Console.WriteLine("Last Name : " + item.Value.LastName);
+                Console.WriteLine("Address : " + item.Value.Address);
+                Console.WriteLine("City : " + item.Value.City);
+                Console.WriteLine("State : " + item.Value.State);
+                Console.WriteLine("Email : " + item.Value.Email);
+                Console.WriteLine("Zip : " + item.Value.Zip);
+                Console.WriteLine("Phone Number : " + item.Value.PhoneNumber + "\n");
+            }
+        }
+
+        public void EditContact(string name, string bookName)
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBookDictionary[bookName].contacts)
+            {
+                if (item.Key == name)
+                {
+                    Console.WriteLine("Choose What to Edit \n1.First Name \n2.Last Name \n3.Address \n4.City \n5.State \n6.Email \n7.Zip \n8.Phone Number");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter New First Name :");
+                            item.Value.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter New Last Name :");
+                            item.Value.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter New Address :");
+                            item.Value.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter New City :");
+                            item.Value.City = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter New State :");
+                            item.Value.State = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter New Email :");
+                            item.Value.Email = Console.ReadLine();
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter New Zip :");
+                            item.Value.Zip = Console.ReadLine();
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter New Phone Number :");
+                            item.Value.PhoneNumber = Console.ReadLine();
+                            break;
+                    }
+                    Console.WriteLine("\nEdited Successfully\n");
+                }
+            }
+        }
+
+        public void DeleteContact(string name, string bookName)
+        {
+            Console.WriteLine("Delete a contact");
+            Console.WriteLine("----------------");
+
+            if (addressBookDictionary[bookName].contacts.ContainsKey(name))
+            {
+                addressBookDictionary[bookName].contacts.Remove(name);
+                Console.WriteLine("\nDeleted Succesfully.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nNot Found, Try Again.\n");
+            }
+        }
+
+        public void AddAddressBook(string bookName)
+        {
+            ContactManager addressBook = new ContactManager();
+            addressBookDictionary.Add(bookName, addressBook);
+            Console.WriteLine("AddressBook Created.");
+        }
+        public Dictionary<string, ContactManager> GetAddressBook()
+        {
+            return addressBookDictionary;
+        }
+        public void NoDuplicateEntry()
+        {
+            List<Contact> addresses = new List<Contact>();
+            Console.WriteLine("Enter name to check duplicate name or not");
+            string firstname = Console.ReadLine();
+            foreach (var data in addressBookDictionary)
+            {
+                if (data.FirstName == firstname)
+                {
+                    Console.WriteLine("Addrss already Present");
+                }
+                else
+                {
+                    AddContact();
+                }
+            }
+        }
+
+    }
+}
