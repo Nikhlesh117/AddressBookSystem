b32e7b3107563a8b62d4e8003634fd00da76d2f2
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-11 12:37:28 +0530

    Added: Reading/Writing To CSV File

---
+++ CSVHandler.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class CSVHandler
+    {
+        private string filePath = @"D:\Work\Bridgelabz\.net repo\AddressBookSystem\AddressBookSystem\AddressBookCSV.csv";
+        public void WriteToFile(Dictionary<string, ContactManager> addressBookDictionary)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    foreach (ContactManager obj in addressBookDictionary.Values)
+                    {
+                        List<Contact> contactRecord = obj.contacts.Values.ToList();
+                        csv.WriteRecords(contactRecord);
+                    }
+                    Console.WriteLine("\nSuccessfully added to CSV file.");
+                    csv.Dispose();
+                }
+            }
+        }
+        public void ReadFromFile()
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    Console.WriteLine("Below are Contents of CSV File");
+                    List<Contact> contactRecord = csv.GetRecords<Contact>().ToList();
+                    foreach (Contact contact in contactRecord)
+                    {
+                        Console.WriteLine(contact.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class FileOperation
+    {
+        private string filePath = @"D:\Work\Bridgelabz\.net repo\AddressBookSystem\AddressBookSystem\AddressBookRecord.txt";
+        public void WriteToFile(Dictionary<string, ContactManager> addressBookDictionary)
+        {
+            using StreamWriter writer = new StreamWriter(filePath, true);
+            foreach (ContactManager addressBookobj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in addressBookobj.contacts.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of Text File");
+            string lines = File.ReadAllText(filePath);
+            Console.WriteLine(lines);
+        }
+    }
+}
